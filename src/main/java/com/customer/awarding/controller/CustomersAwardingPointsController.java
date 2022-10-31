688e8c974a2c30baba31b5ea5e2d29b3af06aa25
sathishdoppala <117023628+sathishdoppala@users.noreply.github.com>
2022-10-31 11:53:00 -0400

    Update CustomersAwardingPointsController.java

--- src/main/java/com/customer/awarding/controller/CustomersAwardingPointsController.java
+++ src/main/java/com/customer/awarding/controller/CustomersAwardingPointsController.java
@@ -1,18 +1,20 @@
 package com.customer.awarding.controller;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.customer.awarding.model.CustomerAwardingPoints;
 import com.customer.awarding.model.CustomerTransactions;
 import com.customer.awarding.model.HealthCheck;
 import com.customer.awarding.service.CustomersAwardingPointsService;
 import com.customer.awarding.service.CustomersAwardingPointsServiceImpl;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.text.ParseException;
 
 /**
  * @author Doppala Satish
@@ -39,7 +41,7 @@ public class CustomersAwardingPointsController {
      *
      * @return
      */
-    @PostMapping(path="/awarding/points", produces = "application/json")
+    @RequestMapping(path="/awarding/points", method = RequestMethod.POST, produces={"application/json"})
     public ResponseEntity<CustomerAwardingPoints> getCustomerAwardingPoints(
             @RequestBody CustomerTransactions customerTransactions) throws ParseException {
 
